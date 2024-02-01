c3791f03383c3e94d3dc4ea0d33ae4a700155043
CharlotteHicken <charlotte.hicken@gmail.com>
2024-02-01 10:52:48 -0500

    The plane shrinks and is then destroyed when spacebar is pressed

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -11,6 +11,8 @@ public class Plane : MonoBehaviour
     Vector2 currentPosition;
     Rigidbody2D rigidbody;
     public float speed = 1.0f;
+    public AnimationCurve landing;
+    float landingTimer;
 
     private void Start()
     {
@@ -32,8 +34,20 @@ public class Plane : MonoBehaviour
         rigidbody.MovePosition(rigidbody.position + (Vector2)(transform.up) * speed * Time.deltaTime);
     }
 
+
     private void Update()
     {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            landingTimer += 0.5f * Time.deltaTime;
+            float interpolation = landing.Evaluate(landingTimer);
+            if (transform.localScale.z < 0.1f)
+            {
+                Destroy(gameObject);
+            }
+            transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, interpolation);
+        }
+
         lineRenderer.SetPosition(0, transform.position);
         if (points.Count > 0 )
         {
