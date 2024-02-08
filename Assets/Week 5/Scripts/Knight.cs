23b3c9e47f130d8758e4f8849d6db2eea8b12195
hickecha <hickecha@sheridancollege.ca>
2024-02-08 11:16:17 -0500

    Made a healthbar and heal button

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -13,6 +13,8 @@ public class Knight : MonoBehaviour
     bool clickingOnSelf = false;
     public float health;
     public float maxHealth = 5;
+    bool isDead = false;
+    public HealthBar healthBar;
 
     // Start is called before the first frame update
     void Start()
@@ -24,18 +26,19 @@ public class Knight : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if(isDead) return;
         movement = destination - (Vector2)transform.position;
         if(movement.magnitude < 0.1)
         {
             movement = Vector2.zero;
         }
         rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(isDead) return;
         if (Input.GetMouseButtonDown(0) && !clickingOnSelf)
         {
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -47,8 +50,10 @@ public class Knight : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if (isDead) return;
         clickingOnSelf = true;
         TakeDamage(1);
+        healthBar.TakeDamage(1);
     }
 
     private void OnMouseUp()
@@ -56,16 +61,18 @@ public class Knight : MonoBehaviour
         clickingOnSelf = false;
     }
 
-    void TakeDamage(float damage)
+    public void TakeDamage(float damage)
     {
         health -= damage;
         health = Mathf.Clamp(health, 0, maxHealth);
         if (health == 0)
         {
+            isDead = true;
             animator.SetTrigger("Death");
         }
         else
         {
+            isDead = false;
             animator.SetTrigger("TakeDamage");
         }
        
