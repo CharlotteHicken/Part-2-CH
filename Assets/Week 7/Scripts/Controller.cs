97c7960aed17a2a428ad64abddb28b1b34e5f19f
hickecha <hickecha@sheridancollege.ca>
2024-02-22 11:19:15 -0500

    Task2 complete: Flicking selected player now works

--- Assets/Week 7/Scripts/Controller.cs
+++ Assets/Week 7/Scripts/Controller.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
+    public Slider chargeSlider;
+    float chargeValue;
+    public float MaxCharge = 1000;
+    Vector2 direction;
+
     public static FootballPlayer SelectedPlayer { get; private set; }
 
     public static void SetSelectedPlayer(FootballPlayer player)
@@ -15,5 +21,38 @@ public class Controller : MonoBehaviour
 
         SelectedPlayer = player;
         SelectedPlayer.Selected(true);
+    }
+
+    private void FixedUpdate()
+    {
+        if (direction != Vector2.zero)
+        {
+            SelectedPlayer.Move(direction);
+            direction = Vector2.zero;
+            chargeValue = 0;
+            chargeSlider.value = chargeValue;
+        }
+    }
+
+    private void Update()
+    {
+        if (SelectedPlayer == null) return;
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            chargeValue = 0;
+            direction = Vector2.zero;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            chargeValue += Time.deltaTime;
+            chargeValue = Mathf.Clamp(chargeValue, 0, MaxCharge);
+            chargeSlider.value = chargeValue;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            direction = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)SelectedPlayer.transform.position).normalized * chargeValue;
+
+        }
     }
 }
--- Assets/Week 7/Scripts/FootballPlayer.cs
+++ Assets/Week 7/Scripts/FootballPlayer.cs
@@ -7,10 +7,13 @@ public class FootballPlayer : MonoBehaviour
 {
     SpriteRenderer sr;
     Color baseColor;
+    Rigidbody2D rb;
+    public float speed = 15;
 
     private void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
         baseColor = sr.color;
         Selected(false);
     }
@@ -35,5 +38,10 @@ public class FootballPlayer : MonoBehaviour
         {
             sr.color = baseColor;
         }
+    }
+
+    public void Move(Vector2 direction)
+    {
+        rb.AddForce(direction * speed);
     }
 }
